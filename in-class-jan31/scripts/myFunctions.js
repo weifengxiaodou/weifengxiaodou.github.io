381256eae09bf066652d99fb4df8e71685ef3157
Melody Hou <houyunjia0531@gmail.com>
2023-02-06 20:29:38 -0500

    hw4

---
+++ in-class-jan31/scripts/myFunctions.js
@@ -0,0 +1,18 @@
+// A series of custom functions. This file can be considered
+// as a custom library, and each of the functions below can be
+// called from an html page after this js file has been loaded.
+
+
+
+
+function sumAB(A,B){
+
+    return A+B;
+}
+
+
+function randomPosition(){
+    return Math.random()*140+70;
+}
+
+
